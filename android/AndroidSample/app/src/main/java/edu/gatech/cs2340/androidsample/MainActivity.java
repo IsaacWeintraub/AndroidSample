fe3ea5b43afde0e8aaf2d789f8781696a2c77ff0
Isaac Weintraub <iweintraub3@gatech.edu>
2018-06-05 01:24:15 -0400

    ready for initial release

--- android/AndroidSample/app/src/main/java/edu/gatech/cs2340/androidsample/MainActivity.java
+++ android/AndroidSample/app/src/main/java/edu/gatech/cs2340/androidsample/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.gatech.cs2340.androidsample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+     * This method will be called when the button is pressed.
+     * add JavaDocs to your heart's content
+     */
+    public void doSomething(View view) {
+        Intent intent = new Intent(this, NextActivity.class);
+        EditText text = (EditText) findViewById(R.id.editText);
+        String name = text.getText().toString();
+        intent.putExtra("NAME", name);
+        startActivity(intent);
     }
 }
